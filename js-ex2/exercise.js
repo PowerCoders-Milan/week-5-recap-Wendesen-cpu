f07f34a657e8422c9e284ed6363a5a1adb12563f
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-03-05 19:00:13 +0100

    Done

--- js-ex2/exercise.js
+++ js-ex2/exercise.js
@@ -9,9 +9,9 @@ var Array2 = [21, 19, 12, 'sebastian', 'will', 23, 20];
 Array1.forEach(item =>{
     if(typeof(item) == "number"){
         var i = Array1.indexOf(item)
-        var tempo = Array2[i];
+        var temp = Array2[i];
         Array2[i] = item;
-        Array1[i]= tempo;
+        Array1[i]= temp;
     }
 })
 
