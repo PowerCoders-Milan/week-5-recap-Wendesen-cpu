e2d043c4b042683b0916bbd6c6a1eb5f742f0f8a
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-03-05 13:09:36 +0100

    Done

--- js-ex3/exercise.js
+++ js-ex3/exercise.js
@@ -7,6 +7,17 @@ formula(5) //=> 6
 formula() //=> 2
 
 // review all the possible parameters
-function formula(){
+function formula(a,b,c){
+
+    if(a === undefined && b === undefined && c === undefined)
+        return 2;
+    else if(c === undefined && b === undefined)
+        return a+1;
+    else if(c === undefined)
+        return (a*b) + 1;
+    else 
+        return (a*b)+c;
 
 }
+
+console.log(formula());
